5f595f961f879c6189d222449e264858439f73eb
Icky <ascenderr11@gmail.com>
2025-06-10 21:32:30 +0300

    feat(review): add validation to DTO and enable ValidationPipe
    - Added class-validator decorators, enabled ValidationPipe, updated dependencies

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,7 +1,20 @@
+import { IsNumber, IsString, Max, Min } from 'class-validator';
+
 export class CreateReviewDto {
+  @IsString({ message: 'Имя должно быть строкой' })
   name: string;
+
+  @IsString({ message: 'Заголовок должен быть строкой' })
   title: string;
+
+  @IsString({ message: 'Описание должно быть строкой' })
   description: string;
+
+  @Min(1, { message: 'Рейтинг не может быть менее 1' })
+  @Max(5, { message: 'Рейтинг не может быть более 5' })
+  @IsNumber({}, { message: 'Рейтинг должен быть числом' })
   rating: number;
+
+  @IsString({ message: 'ID продукта должен быть строкой' })
   productId: string;
 }
