b6d99dd79bf90396853e0c4e83d789c8965d1616
Icky <ascenderr11@gmail.com>
2025-06-11 17:07:32 +0300

    chore(auth): integrate PassportModule and JwtStrategy into AuthModule
    - Imported PassportModule and added it to AuthModule imports
    - Added JwtStrategy to providers
    - Included ConfigModule for configuration loading

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,13 +6,17 @@ import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { getJwtConfig } from 'src/configs/jwt.config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     JwtModule.registerAsync({ imports: [ConfigModule], inject: [ConfigService], useFactory: getJwtConfig }),
+    PassportModule,
+    ConfigModule,
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
