7154569bed9f3b68ea90cf1f9e145a3c81e0985b
Icky <ascenderr11@gmail.com>
2025-06-14 19:18:26 +0300

    feat(pipes): add IdValidationPipe for ObjectId validation in routes
    - Created IdValidationPipe to validate MongoDB ObjectId values in route parameters
    - Applied the pipe to relevant routes in Product and Review controllers
    - Added centralized error message constant for invalid id format

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -16,6 +16,7 @@ import { Product } from './product.schema';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ProductService } from './product.service';
 import { PRODUCT_NOT_FOUND_ERROR } from './product.constants';
+import { IdValidationPipe } from 'src/pipes/id-validation.pipe';
 
 @Controller('product')
 export class ProductController {
@@ -27,7 +28,7 @@ export class ProductController {
   }
 
   @Get(':id')
-  async get(@Param('id') id: string) {
+  async get(@Param('id', IdValidationPipe) id: string) {
     const product = await this.productService.findById(id);
     if (!product) {
       throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
@@ -36,7 +37,7 @@ export class ProductController {
   }
 
   @Delete(':id')
-  async delete(@Param('id') id: string) {
+  async delete(@Param('id', IdValidationPipe) id: string) {
     const deletedProduct = await this.productService.deleteById(id);
     if (!deletedProduct) {
       throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
@@ -44,7 +45,7 @@ export class ProductController {
   }
 
   @Patch(':id')
-  async patch(@Param('id') id: string, @Body() dto: Product) {
+  async patch(@Param('id', IdValidationPipe) id: string, @Body() dto: Product) {
     const updatedProduct = await this.productService.updateById(id, dto);
     if (!updatedProduct) {
       throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
